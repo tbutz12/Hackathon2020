fbd1d1bbb7df7bae685b38fec7e8c23ae04f8971
tbutz12 <tmb132@pitt.edu>
2020-02-15 15:48:56 -0500

    First commit

--- hacky.js
+++ hacky.js
@@ -6,62 +6,48 @@ import Radar from "radar-sdk-js";
 import express from "express";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [ ... ];
+  const plugins = [ ... ];
+
+  return {
+    presets,
+    plugins
+  };
+}
+require.resolve("@babel/core")
+browserify().transform("babelify", {
+  global: true,
+  ignore: /\/node_modules\/(?!app\/)/
+});
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-
   res.sendFile(__dirname + "/index.html");
-
 });
 app.post("/", function(req, res){
   var userName = req.body.userName;
   var trackedName = req.body.trackedName;
   console.log(userName);
   console.log(trackedName);
-  geoFindMe();
 
 });
-app.set('port', process.env.PORT || 3018);
+app.set('port', process.env.PORT || 3033);
 app.listen(app.get('port'), function() {
-  if(app.get('port') == 3018){
-  console.log("Server started on port 3018");
+  if(app.get('port') == 3033){
+  console.log("Server started on port 3033");
 }
 else{
   console.log("Server started on port " + process.env.PORT);
 }
 });
 
-function geoFindMe() {
-  const status = document.querySelector('#status');
-  const mapLink = document.querySelector('#map-link');
-
-  mapLink.href = '';
-  mapLink.textContent = '';
-
-  function success(position) {
-    const latitude  = position.coords.latitude;
-    const longitude = position.coords.longitude;
-
-    status.textContent = '';
-    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-  }
-
-  function error() {
-    status.textContent = 'Unable to retrieve your location';
-  }
-
-  if (!navigator.geolocation) {
-    status.textContent = 'Geolocation is not supported by your browser';
-  } else {
-    status.textContent = 'Locating…';
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
-
-}
-
-//document.querySelector('#find-me').addEventListener('click', geoFindMe);
+var mysql = require('mysql');
